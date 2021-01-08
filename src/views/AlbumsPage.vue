9895dc5091e18ce9813517bcdd78b620dde80662
Anastasiia Konstantinovich <na.konsta@gmail.com>
2021-01-08 13:11:05 +0300

    First commit

---
+++ src/views/AlbumsPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h3>Albums Page</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    async mounted() {
+        this.$store.dispatch('albumInfo/getAlbumsData')
+    },
+
+    data: () => ({
+        albumName: '',
+        error: ''
+    }),
+
+    methods: {
+        openAlbumDetail(album) {
+
+        }
+    }
+}
+</script>
